45d8321343cbf247daf5c03988485547e81a32d9
Reagan D Duecker <dueckerr@gmail.com>
2019-05-06 16:34:30 -0600

    added LogoCard.js

---
+++ src/components/logoCard/LogoCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+const LogoCard = props => (
+  <div className="card" onClick={() => props.clickCount(props.id)}>
+    <div className="img-container">
+    <img alt={props.team} src={props.image} />
+    </div>
+  </div>
+
+);
+
+
+export default LogoCard;
--- src/components/logoCard/index.js
+++ src/components/logoCard/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import "./style.css";
+// import React from 'react';
+// import "./style.css";
 
-const LogoCard = props => (
-  <div className="card" onClick={() => props.clickCount(props.id)}>
-    <div className="img-container">
-    <img alt={props.team} src={props.image} />
-    </div>
-  </div>
+// const LogoCard = props => (
+//   <div className="card" onClick={() => props.clickCount(props.id)}>
+//     <div className="img-container">
+//     <img alt={props.team} src={props.image} />
+//     </div>
+//   </div>
 
-);
+// );
 
-export default LogoCard;
+// export default LogoCard;
+
+export { default } from "./LogoCard";
